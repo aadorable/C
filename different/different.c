f74def8c25d6c01d11ad9d0fb55a4bfd34b0e07c
aadorable <1615691183@qq.com>
2018-04-26 17:36:21 +0800

    different

---
+++ different/different.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+
+int main()
+{
+	int arr[] = { 0, 1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1, 0 };
+	int num = arr[0];
+	int i = 0;
+	for (i = 1; i < sizeof(arr) / sizeof(arr[0]);i++)
+	{
+		num ^= arr[i];
+	}
+	printf("different number is %d\n", num);
+	return 0;
+}
