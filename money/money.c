d40e49d81b3673092a9de696ff6ee37e73275ca1
aadorable <1615691183@qq.com>
2018-05-01 23:29:41 +0800

    money

---
+++ money/money.c
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <windows.h>
+#include <assert.h>
+
+////容易找出其规律为喝2*N-1瓶
+//#define MONEY 20
+//int main()
+//{
+//	printf("drink = %d\n", 2 * MONEY - 1);
+//	system("pause");
+//	return 0;
+//}
+
+int drink(int money)
+{
+	assert(money > 0);
+	int num = money;
+	int empty = money;
+	while (empty > 1)
+	{
+		num += empty / 2;
+		empty = empty / 2 + empty % 2;
+	}
+	return num;
+}
+int main()
+{
+	int money = 0;
+	printf("please input your money:");
+	scanf("%d", &money);
+	int num = drink(money);
+	printf("you can drink %d bottles of water!\n", num);
+	system("pause");
+	return 0;
+}
