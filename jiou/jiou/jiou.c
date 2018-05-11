0c4d8f30607a15546aa0a74888296b7b2c1e1f64
aadorable <1615691183@qq.com>
2018-05-11 23:09:20 +0800

    jiou

---
+++ jiou/jiou/jiou.c
@@ -0,0 +1,54 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <windows.h>
+#include <assert.h>
+
+static exchange(int *p, int *q)
+{
+	*p ^= *q;
+	*q ^= *p;
+	*p ^= *q;
+}
+void Exchange(int arr[], int sz)
+{
+	assert(arr);
+	assert(sz > 0);
+	int *p = arr;
+	int *q = arr + sz - 1;
+	while (p < q)
+	{
+		if (!(*p & 1) && (*q & 1))        //偶数的最后一个bit位一定是0；奇数的最后一个bit位一定是1；
+		{
+			exchange(p, q);
+			p++, q--;
+		}
+		if (*p & 1)
+		{
+			p++;
+		}
+		if (!(*q & 1))
+		{
+			q--;
+		}
+	}
+}
+static void Show(int arr[], int sz)    
+{
+	int i = 0;
+	for (i = 0; i < sz; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	printf("\n");
+}
+int main()
+{
+	int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	Show(arr, sz);
+	Exchange(arr, sz);
+	Show(arr, sz);
+	system("pause");
+	return 0;
+}
