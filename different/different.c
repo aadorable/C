fa4dc35b4f740f3ae91bcdde0319ed464048b7dc
aadorable <1615691183@qq.com>
2018-05-12 22:21:16 +0800

    add different

--- different/different.c
+++ different/different.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 
 int main()
@@ -6,7 +5,7 @@ int main()
 	int arr[] = { 0, 1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1, 0 };
 	int num = arr[0];
 	int i = 0;
-	for (i = 1; i < sizeof(arr) / sizeof(arr[0]);i++)
+	for (i = 1; i < sizeof(arr) / sizeof(arr[0]); i++)
 	{
 		num ^= arr[i];
 	}
